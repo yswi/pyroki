Fix joint indexing problem for analytical pose jacobians (#11)
* make terms in actuated_indices unique

* Simplify logic for combining jacobian along actuated indices

* Add some comments

* Remove sink joint

* ruff

* Add rest pose cost to bimanual example

* benchmark: rerun IK benchmark with current PR

* Add mimic scaling factor to analytical jacobian, and add mimic IK example

* Update benchmark numbers

---------

Co-authored-by: brentyi <yibrenth@gmail.com>

examples/pyroki_snippets/_solve_ik_with_multiple_targets.py
@@ -54,31 +54,42 @@ def _solve_ik_jax(
     target_position: jax.Array,
     target_joint_indices: jax.Array,
 ) -> jax.Array:
-    joint_var = robot.joint_var_cls(0)
+    JointVar = robot.joint_var_cls
+
+    # Get the batch axes for the variable through the target pose.
+    # Batch axes for the variables and cost terms (e.g., target pose) should be broadcastable!
+    target_pose = jaxlie.SE3.from_rotation_and_translation(
+        jaxlie.SO3(target_wxyz), target_position
+    )
+    batch_axes = target_pose.get_batch_axes()
+
     factors = [
-        pk.costs.pose_cost(
-            robot,
-            joint_var,
-            jaxlie.SE3.from_rotation_and_translation(
-                jaxlie.SO3(target_wxyz), target_position
-            ),
+        pk.costs.pose_cost_analytic_jac(
+            jax.tree.map(lambda x: x[None], robot),
+            JointVar(jnp.full(batch_axes, 0)),
+            target_pose,
             target_joint_indices,
             pos_weight=50.0,
             ori_weight=10.0,
         ),
+        pk.costs.rest_cost(
+            JointVar(0),
+            rest_pose=JointVar.default_factory(),
+            weight=1.0,
+        ),
         pk.costs.limit_cost(
             robot,
-            joint_var,
+            JointVar(0),
             jnp.array([100.0] * robot.joints.num_joints),
         ),
     ]
     sol = (
-        jaxls.LeastSquaresProblem(factors, [joint_var])
+        jaxls.LeastSquaresProblem(factors, [JointVar(0)])
         .analyze()
         .solve(
             verbose=False,
             linear_solver="dense_cholesky",
-            trust_region=jaxls.TrustRegionConfig(lambda_initial=1.0),
+            trust_region=jaxls.TrustRegionConfig(lambda_initial=10.0),
         )
     )
-    return sol[joint_var]
+    return sol[JointVar(0)]

src/pyroki/_robot_urdf_parser.py
@@ -8,7 +8,7 @@ import numpy as onp
 import yourdfpy
 from jax import Array
 from jax import numpy as jnp
-from jaxtyping import Bool, Float, Int
+from jaxtyping import Float, Int
 from loguru import logger
 
 
@@ -29,7 +29,7 @@ class JointInfo:
     parent_indices: Int[Array, " n_joints"]
     """Index of the parent joint for each joint. Shape: (n_joints,)."""
     actuated_indices: Int[Array, " n_joints"]
-    """Index of the associated actuated joint that drives each joint. Shape: (n_joints,)."""
+    """Index of the associated actuated joint that drives each joint. -1 if mimic joint. Shape: (n_joints,)."""
 
     # Limits for actuated joints.
     lower_limits: Float[Array, " n_act_joints"]
@@ -49,11 +49,11 @@ class JointInfo:
 
     # Mimic joint parameters.
     mimic_multiplier: Float[Array, " n_joints"]
-    """Mimic multiplier for each joint. Shape: (n_joints,)."""
+    """Mimic multiplier for each joint. Shape: (n_joints,). 1.0 if not a mimic joint."""
     mimic_offset: Float[Array, " n_joints"]
-    """Mimic offset for each joint. Shape: (n_joints,)."""
-    is_mimic: Bool[Array, " n_joints"]
-    """Whether each joint is a mimic joint. Shape: (n_joints,)."""
+    """Mimic offset for each joint. Shape: (n_joints,). 0 if not a mimic joint."""
+    mimic_act_indices: Int[Array, " n_joints"]
+    """Index of the actuated joint that is mimicked by each joint. -1 if not a mimic joint. Shape: (n_joints,)."""
 
     _topo_sort_inv: Int[Array, " n_joints"]
     """Inverse topological sort order, mapping sorted joint index to original joint index."""
@@ -72,10 +72,18 @@ class JointInfo:
         value_padded = jnp.concatenate(
             [value_actuated, jnp.zeros((*batch_axes, 1))], axis=-1
         )
-        safe_actuated_indices = jnp.where(
-            self.actuated_indices == -1,
-            self.num_actuated_joints,  # Point to the zero padding
+
+        # Replace mimic indices with the actuated joint index they refer to.
+        replace_mimic_indices = jnp.where(
+            self.mimic_act_indices == -1,
             self.actuated_indices,
+            self.mimic_act_indices,
+        )
+
+        safe_actuated_indices = jnp.where(
+            replace_mimic_indices == -1,
+            self.num_actuated_joints,  # Point to the zero padding.
+            replace_mimic_indices,
         )
 
         # value_referenced contains the value of the joint each joint index refers to.
@@ -207,7 +215,7 @@ class RobotURDFParser:
         actuated_name_list = list[str]()
         mimic_multiplier_list = list[float]()
         mimic_offset_list = list[float]()
-        is_mimic_list = list[bool]()
+        mimic_act_idx_list = list[int]()
 
         # Store limits read directly from URDF for *all* joints -> _eff
         lower_limit_eff_list = list[float]()
@@ -229,11 +237,11 @@ class RobotURDFParser:
 
             # Get the actuated joint index it refers to (could be itself or mimicked).
             # Also get mimic parameters if applicable.
-            act_idx, is_mimic, multiplier, offset = (
+            act_idx, mimic_act_idx, multiplier, offset = (
                 RobotURDFParser._get_act_joint_idx_and_mimic(urdf, joint)
             )
             actuated_idx_list.append(act_idx)
-            is_mimic_list.append(is_mimic)
+            mimic_act_idx_list.append(mimic_act_idx)
             mimic_multiplier_list.append(multiplier)
             mimic_offset_list.append(offset)
             if act_idx != -1:
@@ -248,7 +256,7 @@ class RobotURDFParser:
             velocity_limit_eff_list.append(vel_limit_eff)
 
             # Get directly actuated joint limits.
-            if joint in urdf.actuated_joints and not is_mimic:
+            if joint in urdf.actuated_joints and mimic_act_idx == -1:
                 lower_limit_act_list.append(lower_eff)
                 upper_limit_act_list.append(upper_eff)
                 velocity_limit_act_list.append(vel_limit_eff)
@@ -302,7 +310,7 @@ class RobotURDFParser:
             velocity_limits_all=velocity_limits_eff_arr,
             mimic_multiplier=mimic_multiplier_arr,
             mimic_offset=mimic_offset_arr,
-            is_mimic=jnp.array(is_mimic_list),
+            mimic_act_indices=jnp.array(mimic_act_idx_list),
             _topo_sort_inv=topo_sort_inv_val,
         )
         assert joint_info.twists.shape == (joint_info.num_joints, 6)
@@ -318,7 +326,7 @@ class RobotURDFParser:
         assert joint_info._topo_sort_inv.shape == (joint_info.num_joints,)
         assert joint_info.mimic_multiplier.shape == (joint_info.num_joints,)
         assert joint_info.mimic_offset.shape == (joint_info.num_joints,)
-        assert joint_info.is_mimic.shape == (joint_info.num_joints,)
+        assert joint_info.mimic_act_indices.shape == (joint_info.num_joints,)
 
         link_info = LinkInfo(
             num_links=len(link_name_list),
@@ -331,23 +339,23 @@ class RobotURDFParser:
     @staticmethod
     def _get_act_joint_idx_and_mimic(
         urdf: yourdfpy.URDF, joint: yourdfpy.Joint
-    ) -> tuple[int, bool, float, float]:
+    ) -> tuple[int, int, float, float]:
         """Get the index of the actuated joint for a joint, and mimic parameters."""
-        is_mimic = False
+        mimic_act_idx = -1
         multiplier = 1.0
         offset = 0.0
 
         # Check if this joint is a mimic joint.
         if joint.mimic is not None:
-            is_mimic = True
             if joint.mimic.multiplier is not None:
                 multiplier = joint.mimic.multiplier
             if joint.mimic.offset is not None:
                 offset = joint.mimic.offset
 
+            act_joint_idx = -1
             mimicked_joint_name = joint.mimic.joint
             mimicked_joint = urdf.joint_map[mimicked_joint_name]
-            act_joint_idx = urdf.actuated_joints.index(mimicked_joint)
+            mimic_act_idx = urdf.actuated_joints.index(mimicked_joint)
 
         # If not mimic, check if it's directly actuated.
         elif joint in urdf.actuated_joints:
@@ -358,7 +366,7 @@ class RobotURDFParser:
         else:
             act_joint_idx = -1  # Represents non-actuated/fixed.
 
-        return act_joint_idx, is_mimic, multiplier, offset
+        return act_joint_idx, mimic_act_idx, multiplier, offset
 
     @staticmethod
     def _get_joint_twist(joint: yourdfpy.Joint) -> Array:

src/pyroki/costs/_pose_cost_analytic_jac.py
@@ -11,29 +11,50 @@ from .._robot import Robot
 def _get_actuated_joints_applied_to_target(
     robot: Robot,
     target_joint_idx: jax.Array,
-):
-    """Get the indices of the actuated joints that affect the pose of the target link.
+) -> jax.Array:
+    """For each joint `i` in the robot, we return an index that is:
 
-    We return an array with shape (num_joints,), where each element is either
-    (a) an actuated joint index or (b) -1 if the joint is not in the path to
-    the target link.
+    1) -1 if the joint is not in the path to the target link.
+    2) an actuated joint index if the joint is in the path to the target link.
+        - If the joint `i` is actuated, this is just `i`.
+        - If the joint `i` mimics joint `j`, this is set to `j`.
+
+    The inputs and outputs should both be integer arrays of shape
+    `(num_joints,)`.
     """
 
-    def scan_fn(current_parent_idx: jax.Array, i: jax.Array):
-        # Check if this joint is in the path to the target
-        is_in_path = i == current_parent_idx
-        act_joint_idx = jnp.where(is_in_path, robot.joints.actuated_indices[..., i], -1)
-        current_parent_idx = jnp.where(
-            is_in_path, robot.joints.parent_indices[..., i], current_parent_idx
-        )
-        return current_parent_idx, act_joint_idx
+    assert target_joint_idx.shape == ()
 
-    return jax.lax.scan(
-        scan_fn,
-        init=target_joint_idx,
-        xs=jnp.arange(robot.joints.num_joints),
-        reverse=True,
-    )[1]
+    def body_fun(joint_idx, indices):
+        # Find the corresponding active actuated joint.
+        active_act_joint = jnp.where(
+            robot.joints.actuated_indices[joint_idx] != -1,
+            # The current joint is actuated.
+            robot.joints.actuated_indices[joint_idx],
+            # The current joint is not actuated; this is -1 if not a mimic joint.
+            robot.joints.mimic_act_indices[joint_idx],
+        )
+
+        # Find the parent of the current joint.
+        parent_joint = robot.joints.parent_indices[joint_idx]
+
+        # Continue traversing up the kinematic tree, using the parent joint.
+        # This value may either go up or down, since there's no guarantee that
+        # the kinematic tree is topologically sorted. :-)
+        next_indices = indices.at[joint_idx].set(active_act_joint)
+        return (parent_joint, next_indices)
+
+    idx_applied_to_target = jnp.full(
+        (robot.joints.num_joints,),
+        fill_value=-1,
+        dtype=jnp.int32,
+    )
+    idx_applied_to_target = jax.lax.while_loop(
+        lambda carry: jnp.any(carry[0] >= 0),
+        lambda carry: body_fun(*carry),
+        (target_joint_idx, idx_applied_to_target),
+    )[-1]
+    return idx_applied_to_target
 
 
 _PoseCostJacCache = tuple[jax.Array, jax.Array, jaxlie.SE3]
@@ -118,19 +139,19 @@ def _pose_cost_jac(
 
     R_ee_world = T_world_ee.rotation().inverse()
 
-    # Get joint twists
-    joint_twists = robot.joints.twists
+    # Get joint twists; these are scaled for mimic joints.
+    joint_twists = robot.joints.twists * robot.joints.mimic_multiplier[..., None]
 
-    # Get angular velocity components (omega)
-    omega_local = joint_twists[..., 3:]
+    # Get angular velocity components (omega).
+    omega_local = joint_twists[:, 3:]
     omega_wrt_world = Ts_world_joint.rotation() @ omega_local
     omega_wrt_ee = R_ee_world @ omega_wrt_world
 
-    # Get linear velocity components (v)
-    vel_local = joint_twists[..., :3]
+    # Get linear velocity components (v).
+    vel_local = joint_twists[:, :3]
     vel_wrt_world = Ts_world_joint.rotation() @ vel_local
 
-    # Compute the linear velocity component (v = ω × r + v_joint)
+    # Compute the linear velocity component (v = ω × r + v_joint).
     vel_wrt_world = (
         jnp.cross(
             omega_wrt_world,
@@ -140,7 +161,7 @@ def _pose_cost_jac(
     )
     vel_wrt_ee = R_ee_world @ vel_wrt_world
 
-    # Combine into spatial jacobian
+    # Combine into spatial Jacobian.
     jac = jnp.where(
         joints_applied_to_target[:, None] != -1,
         jnp.concatenate(
@@ -154,11 +175,15 @@ def _pose_cost_jac(
     ).T
     jac = pose_error.jlog() @ jac
 
-    # TODO: @cmk I don't really know how to correctly use these 🥲
+    # Jacobian of all joints => Jacobian of actuated joints.
     #
-    # this indexing/slicing here works for Panda but I'm not sure how to
-    # make this generalize
-    jac = jac[:, robot.joints.actuated_indices][:, : robot.joints.num_actuated_joints]
+    # Because of mimic joints, the Jacobian terms from multiple joints can be
+    # applied to a single actuated joint. This is summed!
+    jac = (
+        jnp.zeros((6, robot.joints.num_actuated_joints))
+        .at[:, joints_applied_to_target]
+        .add((joints_applied_to_target[None, :] != -1) * jac)
+    )
 
     # Apply weights
     weights = jnp.array([pos_weight] * 3 + [ori_weight] * 3)
@@ -184,7 +209,7 @@ def _pose_cost_analytical_jac(
     Ts_world_joint = robot._forward_kinematics_joints(joint_cfg)
     Ts_world_link = robot._link_poses_from_joint_poses(Ts_world_joint)
 
-    T_world_ee = jaxlie.SE3(Ts_world_link[..., target_link_index, :])
+    T_world_ee = jaxlie.SE3(Ts_world_link[target_link_index, :])
     pose_error = target_pose.inverse() @ T_world_ee
     return (
         pose_error.log() * jnp.array([pos_weight] * 3 + [ori_weight] * 3),